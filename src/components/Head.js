6fed67e0a70a19146bc8b988d6aba8040b36d5cc
rainbowOutlaw <148523561+rainbowOutlaw@users.noreply.github.com>
2024-02-02 15:21:12 +0530

    feat: create and update header component

--- src/components/Head.js
+++ src/components/Head.js
@@ -2,20 +2,34 @@ import React from "react";
 
 const Head = () => {
   return (
-    <div>
-      <img
-        alt="hamburger menu"
-        src="https://cdn.iconscout.com/icon/free/png-256/free-hamburger-menu-462145.png?f=webp"
-      />
-      <img
-        alt="youtube icon"
-        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTGl7Fixf9m5OZ25FPo5RuEdKNSSRUqtqxJeA&usqp=CAU"
-      />
-      <input type="text" />
-      <img
-        alt="user icon"
-        src="https://cdn-icons-png.flaticon.com/512/1144/1144760.png"
-      />
+    <div className="grid grid-flow-col p-2 m-2">
+      <div className="col-span-1 flex gap-2">
+        <img
+          className="h-10"
+          alt="hamburger menu"
+          src="https://cdn.iconscout.com/icon/free/png-256/free-hamburger-menu-462145.png?f=webp"
+        />
+        <img
+          className="h-10"
+          alt="youtube icon"
+          src="https://t3.ftcdn.net/jpg/03/00/38/90/360_F_300389025_b5hgHpjDprTySl8loTqJRMipySb1rO0I.jpg"
+        />
+      </div>
+      <div className="col-span-10 flex justify-center">
+        <input
+          type="text"
+          className="w-1/2 py-1 pl-4 rounded-l-full border-[1px] border-gray-300"
+        />
+        <button className="rounded-r-full bg-gray-100 py-3 p-4 hover:bg-gray-200">
+          Search
+        </button>
+      </div>
+      <div className="col-span-1 w-5 pt-2">
+        <img
+          alt="user icon"
+          src="https://cdn-icons-png.flaticon.com/512/1144/1144760.png"
+        />
+      </div>
     </div>
   );
 };
