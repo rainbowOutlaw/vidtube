ae63be3009d54e009558587fdfe14a3dc7a4cbb4
rainbowOutlaw <148523561+rainbowOutlaw@users.noreply.github.com>
2024-02-02 09:05:57 +0530

    feat: construct header component

---
+++ src/components/Head.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+const Head = () => {
+  return (
+    <div>
+      <img
+        alt="hamburger menu"
+        src="https://cdn.iconscout.com/icon/free/png-256/free-hamburger-menu-462145.png?f=webp"
+      />
+      <img
+        alt="youtube icon"
+        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTGl7Fixf9m5OZ25FPo5RuEdKNSSRUqtqxJeA&usqp=CAU"
+      />
+      <input type="text" />
+      <img
+        alt="user icon"
+        src="https://cdn-icons-png.flaticon.com/512/1144/1144760.png"
+      />
+    </div>
+  );
+};
+
+export default Head;
