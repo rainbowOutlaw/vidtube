dd37a93adbecd63addaf2bd38b83193032db66ba
rainbowOutlaw <148523561+rainbowOutlaw@users.noreply.github.com>
2024-02-05 12:54:20 +0530

    feat: add watch page for each video

--- src/components/WatchPage.js
+++ src/components/WatchPage.js
@@ -1,7 +1,30 @@
 import React from "react";
+import { useDispatch } from "react-redux";
+import { closeMenu } from "../utils/appSlice";
+import { useSearchParams } from "react-router-dom";
 
 const WatchPage = () => {
-  return <div>WatchPage</div>;
+  const dispatch = useDispatch();
+  React.useEffect(() => {
+    dispatch(closeMenu());
+  }, []);
+
+  const [searchParams] = useSearchParams();
+  console.log(searchParams.get("v"));
+
+  return (
+    <div className="px-5">
+      <iframe
+        width="900"
+        height="450"
+        src={"https://www.youtube.com/embed/" + searchParams.get("v")}
+        title="YouTube video player"
+        frameBorder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        allowFullScreen
+      ></iframe>
+    </div>
+  );
 };
 
 export default WatchPage;
